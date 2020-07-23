62ec0e71772fdb94affd728cc78bf2537c335e19
Gigaby7e <skoromniyroma@gmail.com>
2020-07-24 00:42:16 +0300

    Player and camera movement by touch controllers

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 
 namespace Player
 {
     public class PlayerMovementController : MonoBehaviour
     {
         [SerializeField] private Transform _camera;
+        [SerializeField] private CinemachineFreeLook _cinemachineCameraFreeLook;
         [SerializeField] private CharacterController _characterController;
+
+        [SerializeField] private SimpleTouchController _leftController;
+        [SerializeField] private SimpleTouchController _rightController;
+
         [SerializeField] private float _speed = 10f;
 
         private float turnSmoothTime = 0.1f;
@@ -14,23 +20,26 @@ namespace Player
         private void Update()
         {
             InputFoMove();
+
+            _cinemachineCameraFreeLook.m_XAxis.m_InputAxisValue = _rightController.GetTouchPosition.x;
+            _cinemachineCameraFreeLook.m_YAxis.m_InputAxisValue = _rightController.GetTouchPosition.y;
         }
 
         private void InputFoMove()
         {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
+            float horizontal = _leftController.GetTouchPosition.x;
+            float vertical = _leftController.GetTouchPosition.y;
 
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
             if (direction.magnitude >= 0.1f)
             {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-                //Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                _characterController.Move(direction * _speed * Time.deltaTime); 
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _characterController.Move(moveDirection.normalized * _speed * Time.deltaTime); 
             }
         }
     }
